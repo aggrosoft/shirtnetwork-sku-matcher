ea2412be5f6524cb0e7e6c67e5e6fcea86339a70
alexander <coding@aggrosoft.de>
2020-04-21 09:49:25 +0200

    missing metadata version

--- copy_this/snwskumatcher/metadata.php
+++ copy_this/snwskumatcher/metadata.php
@@ -1,10 +1,13 @@
 <?php
+
+$sMetadataVersion = '1.1';
+
 $aModule = array(
     'id'           => 'snwskumatcher',
     'title'        => 'Shirtnetwork Sku Matcher',
     'description'  => 'Mapped Shirtnetwork Artikelnummern zu Oxid Artikelnummern',
     'thumbnail'    => '',
-    'version'      => '1.0.0',
+    'version'      => '1.0.1',
     'author'       => 'Aggrosoft',
     'extend'      => array(
         'oxarticle' => 'snwskumatcher/extensions/models/snwskumatcher_oxarticle'
