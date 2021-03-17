f073c67692a653cbc9cd07086182465bbc232e0b
alexander <coding@aggrosoft.de>
2021-03-17 10:42:31 +0100

    add settings translation

---
+++ copy_this/snwskumatcher/application/views/admin/de/snwskumatcher_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$sLangName = "Deutsch";
+$iLangNr = 0;
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset' => 'UTF-8',
+    'SHOP_MODULE_GROUP_snwskumatcher_settings' => 'Einstellungen',
+    'SHOP_MODULE_aSmartyVariantSkuMatcher' => 'Varianten Matcher',
+    'SHOP_MODULE_aSmartySizeSkuMatcher' => 'Größen Matcher',
+    'SHOP_MODULE_aSmartyPrinttypeSkuMatcher' => 'Druckart Matcher'
+);
